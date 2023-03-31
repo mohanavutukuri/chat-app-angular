07f773687133612bc3bfa8809f1a01b3cd2c9231
mohanavutukuri <mohana.vutukuri@accolitedigital.com>
2023-03-31 14:40:01 +0530

    user profiles added for click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,54 @@
 import { Component } from '@angular/core';
 import { ChatService } from './chat.service';
 
+export interface user{
+  id:number;
+  name:string;
+  age:number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'chat-application';
 
   currId={} as {id:number,enable:boolean};
   newMessage={} as {fromId:number,toId:number,message:string};
   messageList: string[] = [];
-
+  UsersIds:string[]=[];
+  UsersData:user[]=[];
   constructor(private chatService: ChatService){}
 
   ngOnInit(){
     this.currId.enable=false;
   }
+  setReceiver(receiverId:any){
+    this.newMessage.toId=receiverId;
+  }
   registerUser(){
     this.chatService.addToUsers(this.currId.id);
+
+    this.chatService.getNewUsers().subscribe((RevUsers:string[])=>{
+      this.UsersIds=RevUsers.filter(data => this.currId.id.toString()!=data);
+      this.getUserObjects();
+    });
+
     this.currId.enable=true;
     this.newMessage.fromId=this.currId.id;
     this.chatService.getNewMessage().subscribe((data:{fromId:number,message:string}) => {
       if(data.message!=null && data.message!='')
       this.messageList.push(data.message);
     })
+  }
+  getUserObjects(){
+    this.UsersData=[]
+    this.UsersIds.forEach(id=>{
+      this.UsersData.push(this.chatService.getById(id));
+    });
   }
   sendMessage() {
     // this.newMessage.toId=1;
--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -7,14 +7,37 @@ import { io } from "socket.io-client";
 })
 export class ChatService {
 
+  sampleUsers=[
+    {
+      id:1,
+      name:"mohan",
+      age:22
+    },
+    {
+      id:2,
+      name:"snehi",
+      age:21
+    },
+    {
+      id:3,
+      name:"geetha",
+      age:20
+    }
+  ]
   public message$: BehaviorSubject<{fromId:number,message:string}> = new BehaviorSubject({} as {fromId:number,message:string});
-
+  public user$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
   // socket = io('https://node-api-1wbx.onrender.com');
   socket = io('http://localhost:3000');
   constructor() { }
 
-  public addToUsers(id:number){
+  public addToUsers=(id:number)=>{
     this.socket.emit('connection',id);
+  }
+  public getNewUsers=()=>{
+    this.socket.on('connection',(RevUsers:string[])=>{
+      this.user$.next(RevUsers);
+    });
+    return this.user$.asObservable();
   }
   public sendMessage(message: any) {
     console.log('sendMessage: ', message)
@@ -28,4 +51,14 @@ export class ChatService {
 
     return this.message$.asObservable();
   };
+
+  getById(id:any){
+    let user:any={}
+    this.sampleUsers.forEach(data=>{
+      if(data.id==id){
+        user=data;
+      }
+    })
+    return user;
+  }
 }
